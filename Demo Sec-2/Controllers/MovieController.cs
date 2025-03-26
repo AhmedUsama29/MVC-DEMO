6044c9b3dba4b9e81320f74f8a4819a1011b61a0
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-03-26 20:00:08 +0200

    Add project files.

---
+++ Demo Sec-2/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_Sec_2.Controllers
+{
+    public class MovieController: Controller
+    {
+        public string Index(int id)
+        {
+            return $"Hello, {id}";
+        }
+        public IActionResult GetMovie(int? Id, string name)
+        {
+            if (!Id.HasValue || Id == 0)
+                return BadRequest("Invalid Movie ID.");
+            else if (Id < 10)
+                return NotFound("Movie not found.");
+            else
+                return Content($"Movie {Id}, Name: {name}");
+        }
+        public IActionResult TestRedirectToAction()
+        {
+            return Redirect("https://www.facebook.com/");
+        }
+        public IActionResult TestRedirectToGetMovies()
+        {
+            return RedirectToAction("GetMovie", new { id = 10, name = "movie1" });
+            return Redirect("https://localhost:7247/Movies/GetMovie/1?name=dd");        }
+    }
+}
---
+++ Demo Sec-2/Program.cs
@@ -0,0 +1,79 @@
+namespace Demo_Sec_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            app.MapGet("/ ", async context =>
+             {
+                 await context.Response.WriteAsync("Hello nada !");
+             });
+
+            //app.MapGet("/", async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello Test !");
+            //});
+
+            //app.MapGet("/test", async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello Test !");
+            //});
+
+            //app.MapGet(pattern: "/{name}", async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello Test !");
+            //});
+
+            //app.MapGet(pattern: "/{fixed}/{name}", requestDelegate: async context =>
+            //{
+            //    var name = context.GetRouteValue("name");
+            //    var name2 = context.Request.RouteValues["name"];
+            //    await context.Response.WriteAsync($"Hello fixed {name} v1!");
+            //});
+
+            //app.MapGet("/test", async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello Test v2!");
+            //});
+
+            //app.MapGet("/Movie/GetMovie", async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello Get Movie!");
+            //});
+
+            //app.MapControllerRoute(
+            //name: "movie",
+            //pattern: "{controller}/{action}/{id?}"
+            //);
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller}/{action}/{id:regex(^\\d{{2}}$)?}/{mynameAlpha$)}",
+            //    defaults: new { controller = "Home", action = "Index" },
+            //    constraints: new { id = @"\d{2}" }
+            //);
+
+
+            app.Run();
+        }
+    }
+}
